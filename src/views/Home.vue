459244bfd0fd9ee125c19f7948e4abe09366a964
ik1533 <braun@kit.edu>
2022-01-13 13:06:20 +0100

    init app

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Scribe />
+  <Toast position="bottom-right" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Scribe from "@/components/Scribe.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: { Scribe },
+});
+</script>
+
+<style lang="scss" scoped>
+.p-card {
+  margin: 5px;
+}
+</style>
